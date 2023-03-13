7dbcaddc7bc940e1b0f3b7dea2a8c6d80abbd8d0
jarne Van Kerckhove <jarne.vankerckhove@student.kuleuven.be>
2023-03-13 18:46:53 +0100

    localSearch_optim

--- localSearch.py
+++ localSearch.py
@@ -6,11 +6,17 @@ from cost import *
 def carToZone(car: Vehicle, zone: Zone, reservations: List[Reservation]):
     assigned = False
     for reservation in reservations:
-        if reservation.zone == zone.id and reservation.vehiecel is None:
+        #check for own zone to minimise cost
+        if reservation.zone.id == zone.id and reservation.vehiecel is None:
             reservation.vehiecel = car
             car.zone = zone.id
             assigned = True
-            
+        
+        #also assign possible neigbours
+        elif reservation.vehiecel is None and zone.id in reservation.zone.neighbours:
+            reservation.vehiecel = car
+            car.zone = zone.id
+            assigned = True
 
     if not assigned:
         print("not possible to assign vehicle to zone")
@@ -23,3 +29,8 @@ def switchCarToNeighbours(car: Vehicle, reservations: List[Reservation]) -> Vehi
 
 def resToOtherVehicle():
     None
+
+
+def LocalSearch():
+    #take a random number, move the car with that id to a neighbour
+    pass
