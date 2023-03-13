a280bc7716e03e4ff30b6e72774a3de121866f1f
waldo wautelet <waldowautelet4@gmail.com>
2023-03-13 12:16:23 +0100

    cleanup

--- cost.py
+++ cost.py
@@ -1,25 +1,21 @@
 import DataStructure
+from typing import *
 
-def CalculateCosts(reservatorions:list[DataStructure.Reservation],zones:list[DataStructure.Zone]):
+def CalculateCosts(reservatorions: List[DataStructure.Reservation], zones: List[DataStructure.Zone]) -> int:
 
-    p1_tot = 0
-    p2_tot = 0
+    total_cost = 0
     for res in reservatorions:
-        #res not assigned
-        if res.vehiecel == None:
-            res.p1 = 10
-            res.p2 = 5
-            p1_tot += 10
-            p2_tot += 5
-        elif res.vehiecel.zone.id == res.zone:
-            res.p1 = 15
-            res.p2 = 7
-            p1_tot += 15
-            p2_tot += 7
-        elif res.vehiecel.zone.id in zones(res.zone):
-            res.p1 = 5
-            res.p2 = 2
-            p1_tot += 5
-            p2_tot += 2
 
-    return p1_tot,p2_tot
+        # res not assigned
+        if res.vehiecel == None:
+            total_cost += res.p2
+
+        # res assigned car in own zone
+        elif res.vehiecel.zone.id == res.zone:
+            total_cost += 0
+
+        # res assigned in car in neighbouring zone 
+        elif res.vehiecel in zones[res.zone].neighbours:
+            total_cost += res.p1
+
+    return total_cost
