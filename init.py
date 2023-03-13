9b8f7adcea5ffb1efa159e02134a90855b7fe64a
WhereIsW4ldo <waldowautelet4@gmail.com>
2023-03-13 17:57:41 +0100

    cleanup init

--- init.py
+++ init.py
@@ -3,39 +3,33 @@ import pprint
 from DataStructure import *
 from typing import *
 
+def getPossibleAmountCars(reservation: Reservation):
+        return len(reservation.possibleVehicles)
 
-def sortReservaties(reservaties: List[Reservation]):
+def sortNormal(reservation: Reservation):
+        return reservation.id
 
-    def getPossibleAmountCars(reservatie: Reservation):
-        return len(reservatie.possibleVehicles)
-
+def sortReservations(reservation: List[Reservation]):
     # sorteer geinitialiseerde reservaties
-    reservaties.sort(key=getPossibleAmountCars)
-    return reservaties
+    reservation.sort(key=getPossibleAmountCars)
+    return reservation
 
 
 
-def initialise(reservaties: List[Reservation], zones: List[Zone], voertuigen: List[Vehicle], interferences: List[List[bool]]) -> Tuple[List[Reservation], List[Zone]]:
-    # problem: elke auto kan slechts op 1 reservatie staan?
-
-    def sortNormal(reservatie: Reservation):
-        return reservatie.id
+def initialise(reservations: List[Reservation], zones: List[Zone], vehicles: List[Vehicle], interferences: List[List[bool]]) -> Tuple[List[Reservation], List[Zone]]:
     
-    reservaties = sortReservaties(reservaties)
+    # sort reservations from least cars possible to most
+    reservations: List[Reservation] = sortReservations(reservations)
 
     used = []
 
-    for res in reservaties:
+    for res in reservations:
         for posVeh in res.possibleVehicles:
             if posVeh not in used:
                 used.append(posVeh)
-                res.vehicle = voertuigen[posVeh]
-                voertuigen[posVeh].zone = res.zone
-                
-                # for res2 in reservaties:
-                #     if (res2.zone == res.zone) and (res2.vehicle is None) and not (interferences[res.id][res2.id]):
-                #         res2.vehicle = voertuigen[posVeh]
+                res.vehicle = vehicles[posVeh]
+                vehicles[posVeh].zone = res.zone
 
-    reservaties.sort(key=sortNormal)
+    reservations.sort(key=sortNormal)
 
-    return reservaties, voertuigen
+    return reservations, vehicles
