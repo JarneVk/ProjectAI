bb61a121ca37dcc100fb52d8468969b70def2494
jarne Van Kerckhove <jarne.vankerckhove@student.kuleuven.be>
2023-03-18 18:30:53 +0100

    random

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pprint import pprint
 import argparse
 import threading
 import signal
+import random
 
 from ls import LocalSearch
 
@@ -30,6 +31,7 @@ def main():
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('-f', '--file', help='the filepath relative to the main.py file', default=os.path.join('input', 'toy1.csv'))
     parser.add_argument('-t', '--time', help='the amount of time (seconds) the algorithm may run', type=int, default=1)
+    parser.add_argument('-s', '--seed',help='give a seed for the random number generator',type=int,default=69)
 
     args = parser.parse_args()
 
@@ -37,6 +39,8 @@ def main():
 
     x = threading.Thread(target=thread_function, args=(args.time, ))
     x.start()
+
+    random.seed(10)
 
     try:
         start_time = time.perf_counter()
@@ -53,12 +57,13 @@ def main():
 
         print("initial solution: ",ls.checkAll())
 
+        amount_v = len(ls.vehicles)
         while(True):
-            for i in range(len(ls.vehicles)):
-                ls.switchCarToNeighbours(i)
+            #select a random vehicle
+            ls.switchCarToNeighbours(int(random.random()*amount_v))
 
     except Exception as ex:
-        print("Timeout catched!")
+        print(f"exception {ex}")
 
     finally:
         end_time = time.perf_counter()
@@ -68,8 +73,8 @@ def main():
 
         ls.writeOutput(os.path.join("output", os.path.split(args.file)[-1]))
 
-        print(f"init time: {init_time-start_time}")
-        print(f"end  time: {end_time-start_time}")
+        print("init time: {time:.4f}".format(time=(init_time-start_time)))
+        print("end  time: {time:.4f}".format(time=(end_time-start_time)))
 
 if __name__ == "__main__":
     main()
